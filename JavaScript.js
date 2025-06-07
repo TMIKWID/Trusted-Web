e51bcc6abd4e16399915b88474dcfaa546b53f83
TMIKWID <glitchyttg@gmail.com>
2025-06-07 19:50:26 -0400

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,16 @@
+<script>
+  let slideIndex = 0;
+  const slides = document.getElementsByClassName("slide");
 
+  function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.opacity = 0;
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].style.opacity = 1;
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+  }
+
+  window.onload = showSlides;
+</script>
